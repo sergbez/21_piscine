48084bf2181ff1be0468a510fa6ee0cd31ab501d
sergbez <bezru@newmail.ru>
2020-03-27 21:52:44 +0300

    ex07

--- day02/ex07/ft_print_combn.c
+++ day02/ex07/ft_print_combn.c
@@ -1,66 +1,63 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_combn.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: osuggs <osuggs@student.21-school.ru>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/22 17:27:46 by osuggs            #+#    #+#             */
-/*   Updated: 2020/03/22 17:27:46 by osuggs           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
 
-void	display(int *a, int n)
+void    ft_putchar(char c)
 {
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		ft_putchar(a[i] + '0');
-		i++;
-	}
-	if (a[0] < (10 - n))
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
+    write(1, &c, 1);
 }
-
-void	ft_print_combn(int n)
+void ft_print_combn(int n)
 {
-	int	i;
-	int	a[10];
+    int a[9];
+    int i;
 
-	i = 0;
-	while (i < n)
-	{	
-		a[i] = i;
-		i++;
-	}
-	while (a[0] <= (10 - n))
-	{
-		display(a, n);
-		i = n - 1;
-		a[i]++;
-		while (i > 0)
-		{			
-			if (a[i] > 9)
-			{
-				a[i]++;
-				if (a[i] < 10 - n + i)
-				{					
-					while (i < n - 1)
-					{
-						a[i + 1] = a[i] + 1;
-						i++;
-					}
-				}
-			}
-			i--;
-		}		
-	}
-	ft_putchar('\n');
+    i = 0;
+    n = 9;
+    while (i < n)
+    {
+        a[i] = i;
+        i++;
+    }
+    while (a[0] <= 10 - n)
+    {
+        i = 0;
+        while (i < n)
+        {
+            ft_putchar(a[i] + '0');
+            i++;
+        }
+        if (a[0] < 10 - n)
+        {
+            ft_putchar(',');
+            ft_putchar(' ');
+        }
+        if (a[0] == 10 - n)
+            break;
+        i = n - 1;
+        a[i]++;
+        if (a[i] > 9)
+        {
+            i--;
+            while (i >= 0)
+            {
+                if (a[i] < 10 - n + i)
+                {
+                    a[i]++;
+                    while (i < n - 1)
+                    {
+                        a[i + 1] = a[i] + 1;
+                        i++;
+                    }
+                    break;
+                }
+                i--;
+            }
+        }
+    }
+}
+int    main(void)
+{
+    int n;
+    
+    ft_print_combn(n);
+    return(0);
 }
